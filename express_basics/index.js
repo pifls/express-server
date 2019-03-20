8266402e6aa41884e2f6d8c878ebd89d047a7820
Marek Pikosz <marek.pikosz@gmail.com>
2019-03-20 11:05:14 +0100

    Get method for single user

--- express_basics/index.js
+++ express_basics/index.js
@@ -6,10 +6,16 @@ const users  = require('./Users');
 const app = express();
 
 // init Middleware
-app.use(logger);
+// app.use(logger);
 
 // Get all users
 app.get('/api/users', (req, res) => res.json(users));
+
+// Get single user 
+app.get('/api/users/:id', (req, res) => {
+    const user = users.find(user => user.id === parseInt(req.params.id));
+    user ? res.json(user) : res.status(400).json({msg: `No member with id ${req.params.id}`});
+});
 
 // Set a static folder 
 app.use(express.static(path.join(__dirname, 'public')));
